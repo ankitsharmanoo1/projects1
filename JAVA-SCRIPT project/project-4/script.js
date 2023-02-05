3f5a98007ea08bb4fa25abed65201c2767946228
ankitsharmanoo1 <107956361+ankitsharmanoo1@users.noreply.github.com>
2023-02-05 18:00:38 +0530

    File Added

---
+++ JAVA-SCRIPT project/project-4/script.js
@@ -0,0 +1,14 @@
+const messageInput = document.getElementById('message-input');
+// console.log(messageInput);
+
+messageInput.addEventListener("keydown", function(event){
+    // console.log(event);
+    if(event.key=="Enter"){
+        getMessage();
+    }
+})
+
+function getMessage(){
+     document.getElementById("message-output").innerHTML = messageInput.value;
+     messageInput.value = "";
+}
